8c4472bb55aac7b534d6d6bdc30be4b930c1d2a3
Louis Taylor <kragniz@gmail.com>
2014-05-22 16:08:04 +0100

    Add get_IIMWV function

--- rowind.c
+++ rowind.c
@@ -1,5 +1,6 @@
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <termios.h>
 #include <unistd.h>
@@ -8,7 +9,7 @@ int set_serial_options (int fd) {
         struct termios tty;
         memset (&tty, 0, sizeof tty);
         if (tcgetattr (fd, &tty) != 0) {
-                return -1;
+            return -1;
         }
 
         cfsetospeed (&tty, B4800);
@@ -16,7 +17,7 @@ int set_serial_options (int fd) {
 
         tty.c_iflag &= ~IGNBRK;
         tty.c_lflag = 0;
-        tty.c_cc[VMIN]  = 0;
+        tty.c_cc[VMIN]  = 1;
         tty.c_cc[VTIME] = 50;
 
         tty.c_iflag &= ~(IXON | IXOFF | IXANY);
@@ -28,7 +29,7 @@ int set_serial_options (int fd) {
         tty.c_cflag &= ~CRTSCTS;
 
         if (tcsetattr (fd, TCSANOW, &tty) != 0) {
-                return -2;
+            return -2;
         }
         return 0;
 }
@@ -43,13 +44,17 @@ int get_rowind_fd(char* portname) {
     return fd;
 }
 
-int main() {
-    int fd = get_rowind_fd("/dev/ttyUSB0");
+char* get_IIMWV(int ro_fd, char* line) {
     char c;
     int n = 1;
     while (n >= 1) {
-        n = read (fd, &c, 1);
+        n = read (ro_fd, &c, 1);
         printf(&c);
     }
-    return 1;
+}
+
+int main() {
+    int ro_fd = get_rowind_fd("/dev/ttyUSB0");
+    char* line = malloc(30);
+    puts(get_IIMWV(ro_fd, line));
 }
