1fffc8856948fb252cf8e7859f8588f420f7df2e
Louis Taylor <kragniz@gmail.com>
2014-05-25 01:32:17 +0100

    Add setup.py

---
+++ python/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Rowind',
+      version='1.0',
+      description='Library to read from RO Wind sensor',
+      author='Louis Taylor',
+      author_email='kragniz@gmail.com',
+      url='https://github.com/kragniz/librowind',
+      packages=['rowind'],
+     )
